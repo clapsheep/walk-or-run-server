769322648f20eb7ffad88d6e9b4b045343cd9cee
clapsheep <sktndid1203@naver.com>
2024-11-14 17:44:22 +0900

    [init] project initial setting

---
+++ src/main/java/com/wor/WalkOrRunServerApplication.java
@@ -0,0 +1,13 @@
+package com.wor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkOrRunServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkOrRunServerApplication.class, args);
+	}
+
+}
