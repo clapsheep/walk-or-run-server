385fac081b1bcc698547cc89b3844e38390d5719
clapsheep <sktndid1203@naver.com>
2024-11-18 21:44:31 +0900

    [feat] Comment 기능 구현 - CommentController   - createComment, updateChallenge, deleteChallenge 구현   -  CommentMapper.xml 구현

--- src/main/java/com/wor/dash/comment/model/controller/CommentController.java
+++ src/main/java/com/wor/dash/comment/model/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.wor.dash.comment.model.controller;
 
 import java.util.List;
 
+import com.wor.dash.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wor.dash.challenge.model.Challenge;
 import com.wor.dash.comment.model.Comment;
 import com.wor.dash.comment.model.service.CommentService;
 
@@ -23,32 +23,50 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("api/comment")
 public class CommentController {
-	
-	private final CommentService commentService;
-	
-	@PostMapping
-	public ResponseEntity<Void> createComment(@RequestBody Comment comment) {
-		commentService.addComment(comment);
-		return ResponseEntity.status(HttpStatus.CREATED).build();
-	}
-	
-	@GetMapping()
-	public ResponseEntity<List<Comment>> getCommentList() {
+
+    private final CommentService commentService;
+
+    @PostMapping("")
+    public ResponseEntity<?> createComment(@RequestBody Comment comment) {
+        try {
+            commentService.addComment(comment);
+            return new ResponseEntity<>(new ApiResponse("success", "createComment", 200), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse("fail", "createComment", 500), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+    }
+
+    // 구현 필요성? -> 댓글은 Challenge 가져올 때 Join으로 가져오는 것이 낫지 않은가?
+    @GetMapping("")
+    public ResponseEntity<List<Comment>> getCommentList() {
         List<Comment> commentList = commentService.getAllComment();
         return ResponseEntity.ok(commentList);
     }
-	
-	
-	@PutMapping
-    public ResponseEntity<Void> updateChallenge(@RequestBody Comment comment) {
-		commentService.editComment(comment);
-        return ResponseEntity.ok().build();
+
+
+    @PutMapping("")
+    public ResponseEntity<ApiResponse> updateChallenge(@RequestBody Comment comment) {
+        try {
+            commentService.editComment(comment);
+            return new ResponseEntity<>(new ApiResponse("success", "updateComment", 200), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse("fail", "updateComment", 500), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
-	
-	@DeleteMapping("/{commentId}")
-    public ResponseEntity<Void> deleteChallenge(@PathVariable int commentId) {
-        commentService.removeComment(commentId);
-        return ResponseEntity.ok().build();
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<ApiResponse> deleteChallenge(@PathVariable int commentId) {
+        try {
+            commentService.removeComment(commentId);
+            return new ResponseEntity<>(new ApiResponse("success", "deleteComment", 200), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse("fail", "deleteComment", 500), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
     }
-	
+
 }
