9d1a7e7b37bdb3f86615019bcd34d478c0536710
clapsheep <sktndid1203@naver.com>
2024-11-18 22:30:40 +0900

    [feat] 불필요한 test 제거 - test 의존성 제거

--- src/main/java/com/wor/dash/config/SwaggerConfig.java
+++ src/main/java/com/wor/dash/config/SwaggerConfig.java
@@ -11,24 +11,31 @@ import io.swagger.v3.oas.models.info.Info;
 
 //Swagger-UI 확인
 //http://localhost/swagger-ui.html
-//http://localhost:8080/WalkOrRun/swagger-ui.html
+//http://localhost:8080/swagger-ui.html
 
 
 @Configuration
 public class SwaggerConfig {
 
-	@Bean
-	public OpenAPI openAPI() {
-		Info info = new Info().title("WalkOrRun REST API 명세서").description(
-				"REST API를 이용한 WalkOrRun 서비스 기능 명세서")
-				.version("v1").contact(new io.swagger.v3.oas.models.info.Contact().name("JaeSeoHan")
-						.email("hanahyun1@korea.ac.cr").url("https://github.com/clapsheep/WalkOrRun-server"));
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("WalkOrRun REST API 명세서")
+                .description("REST API를 이용한 WalkOrRun 서비스 기능 명세서")
+                .version("v1")
+                .contact(new io.swagger.v3.oas.models.info.Contact()
+                        .name("JaeSeoHan")
+                        .email("hanahyun1@korea.ac.cr").url("https://github.com/clapsheep/WalkOrRun-server"));
 
-		return new OpenAPI().components(new Components()).info(info);
-	}
+        return new OpenAPI().components(new Components()).info(info);
+    }
 
-	@Bean
-	public GroupedOpenApi publicApi() {
-		return GroupedOpenApi.builder().group("wor-api").pathsToMatch("/api/**").build();
-	}
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi
+                .builder()
+                .group("wor-api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
 }
