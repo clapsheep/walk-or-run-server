12b7d70bb85151e8cd252dcaabbaa9d112a34c64
zyu22 <zyu22@naver.com>
2024-11-19 12:42:00 +0900

    1. Challenge 조회 시 진행/종료/전체 별로 조회해오게 API 분리 2. Challenge Swagger API 작성 3. Challenge Dto에 목표인원수 및 참여자 수 조회해오게 변경

--- src/main/java/com/wor/dash/record/model/Record.java
+++ src/main/java/com/wor/dash/record/model/Record.java
@@ -1,11 +1,13 @@
 package com.wor.dash.record.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@RequiredArgsConstructor
 @NoArgsConstructor
 public class Record {
     private int totalCalorie; // 총소모칼로리
@@ -21,5 +23,4 @@ public class Record {
     private int minHeartRate; // 최소 심박수
     private int distance; // 이동 거리(미터)
     private int meanSpeed;
-
 }
