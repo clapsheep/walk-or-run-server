773de8afd51972281614093942f824b71a2ee1b5
YuJeong <hao_yj@naver.com>
2024-11-19 23:26:54 +0900

    csv 데이터 파싱 및 DB 삽입 기능 구현

--- src/main/java/com/wor/dash/record/model/Calorie.java
+++ src/main/java/com/wor/dash/record/model/Calorie.java
@@ -7,78 +7,15 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@RequiredArgsConstructor
-//@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class Calorie {
 	private int calorieId;
 	@NonNull private int timeId;
 	private double totalCalorie;
 	private double calorieDay;
 
-//	public Calorie(int calorieId, @NonNull int timeId, double totalCalorie, double calorieDay) {
-//		this.calorieId = calorieId;
-//		this.timeId = timeId;
-//		this.totalCalorie = totalCalorie;
-//		this.calorieDay = calorieDay;
-//	}
-//
-//	public Calorie(@NonNull int timeId, double totalCalorie, double calorieDay) {
-//		this.timeId = timeId;
-//		this.totalCalorie = totalCalorie;
-//		this.calorieDay = calorieDay;
-//	}
-//
-//	public Calorie(@NonNull int timeId, double calorieDay) {
-//		this.timeId = timeId;
-//		this.calorieDay = calorieDay;
-//	}
-//
-//	public Calorie() {
-//
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "Calorie{" +
-//				"calorieId=" + calorieId +
-//				", timeId=" + timeId +
-//				", totalCalorie=" + totalCalorie +
-//				", calorieDay=" + calorieDay +
-//				'}';
-//	}
-//
-//	public int getCalorieId() {
-//		return calorieId;
-//	}
-//
-//	public void setCalorieId(int calorieId) {
-//		this.calorieId = calorieId;
-//	}
-//
-//	public @NonNull int getTimeId() {
-//		return timeId;
-//	}
-//
-//	public void setTimeId(@NonNull int timeId) {
-//		this.timeId = timeId;
-//	}
-//
-//	public double getTotalCalorie() {
-//		return totalCalorie;
-//	}
-//
-//	public void setTotalCalorie(double totalCalorie) {
-//		this.totalCalorie = totalCalorie;
-//	}
-//
-//	public double getCalorieDay() {
-//		return calorieDay;
-//	}
-//
-//	public void setCalorieDay(double calorieDay) {
-//		this.calorieDay = calorieDay;
-//	}
 }
