73aaa7d95fb9388fae1f77c552202d8d1c71b0ba
zyu22 <zyu22@naver.com>
2024-11-20 15:55:43 +0900

    [refactor] Service 이름 수정 복수형 -> List

--- src/main/java/com/wor/dash/challenge/model/mapper/ChallengeMapper.java
+++ src/main/java/com/wor/dash/challenge/model/mapper/ChallengeMapper.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface ChallengeMapper {
     void insertChallenge(Challenge challenge);
 
-    List<Challenge> selectChallengeList();
+    List<Challenge> selectAllChallengeList();
 
     List<Challenge> selectActiveChallengeList();
 
